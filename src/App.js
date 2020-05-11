400969ec071d909b93765cd538bfa3a81da5963d
Hasan Tareque <hmtareque@gmail.com>
2020-05-11 01:32:19 +0100

    default material admin template

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
-    <div>
-      <p>Sleekie Admin Panel</p>
-    </div>
+    <Dashboard/>
   );
 }
+
 
 export default App;
